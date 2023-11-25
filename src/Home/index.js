4f3000b74c2e805aa32c0c292399b456a9949c0a
arnugroho <nugroho.akbar.r@gmail.com>
2023-11-25 11:24:18 +0700

    update routes

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import {Route, Routes} from 'react-router-dom';
 import ProfileInput from "./StatelessComponent/ProfileInput";
+import Profile from "./StatelessComponent/Profile";
+import TableProfile from "./StatelessComponent/TableProfile";
 
 
 const Main = () => (
     <div>
         <Routes>
-            <Route path="/" element={<ProfileInput/>}>
-                {/*<Route path="profile" element={<ProfileInput/>}/>*/}
-            </Route>
+            <Route path="/" element={<ProfileInput/>}/>
+            <Route path="profile" element={<Profile/>}/>
         </Routes>
 
     </div>
